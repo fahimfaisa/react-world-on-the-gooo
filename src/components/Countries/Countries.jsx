b10a20f6b8d306671acbd9531b57f0abade82e08
Programming Hero <faisaleon1910@gmail.com>
2025-09-20 19:10:17 +0600

    lift unshieft

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { use, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
+  const handleVisitedCountries = (country) => {
+    console.log("handle visited countries clicked", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  const handleVisitedFlag = (flag) => {
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  };
+
   return (
     <div>
-      <h1>In the countries</h1>
+      <h1>In the countries: {countries.length}</h1>
+      <h3>Total Country Visited: {visitedCountries.length} </h3>
+      <h3>Total Flags Visited: {visitedFlags.length}</h3>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li>{country.name.common}</li>
+        ))}
+      </ol>
+      <div className="visited-flags-container">
+        {visitedFlags.map((flag, index) => (
+          <img key={index} src={flag}></img>
+        ))}
+      </div>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+            handleVisitedFlag={handleVisitedFlag}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
